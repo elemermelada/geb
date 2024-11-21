acea15ed58ce22264bba2274970cae7ce3600644
elemermelada <iiasanmi9@gmail.com>
2024-11-21 20:02:10 +0100

    feat: theorem generator working

--- MIU/main.cpp
+++ MIU/main.cpp
@@ -1,4 +1,5 @@
 #include "MIU.h"
+#include "Container.h"
 #include "tests.cpp"
 
 int main()
@@ -6,5 +7,11 @@ int main()
     bool success = runTests();
     if (!success)
         return 1;
+    auto container = Container(MIU("MI"));
+    for (int i = 0; i < 8; i++)
+    {
+        container.iterateCurrent();
+    }
+    container.printChains();
     return 0;
 }
