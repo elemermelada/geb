ae1593bcbc99fd2442e4d3273af9a2fc7bbe648e
elemermelada <iiasanmi9@gmail.com>
2024-11-21 19:57:26 +0100

    chore: improve memory management

--- MIU/MIU.cpp
+++ MIU/MIU.cpp
@@ -24,6 +24,11 @@ MIU &MIU::operator=(MIU from)
     return *this;
 }
 
+bool MIU::operator==(const MIU &oth)
+{
+    return this->checkData(oth.m_data);
+}
+
 MIU::~MIU()
 {
     if (m_data)
@@ -52,20 +57,23 @@ bool MIU::canApplyRule(const Rules rule, int *fromp)
     {
     case Rules::I:
     {
-        if (m_data[m_size - 1] == 'I')
+        if (*fromp == 0 && m_data[m_size - 1] == 'I')
+        {
             return true;
+        }
         return false;
     }
     case Rules::II:
     {
-        if (m_data[0] == 'M' && m_size > 1)
+        if (*fromp == 0 && m_data[0] == 'M' && m_size > 1)
+        {
             return true;
+        }
         return false;
     }
     case Rules::III:
     {
-        int from = fromp ? *fromp : 0;
-        for (int i = from; i < m_size - 2; i++)
+        for (int i = *fromp; i < m_size - 2; i++)
         {
             if (m_data[i] == 'I' && m_data[i + 1] == 'I' && m_data[i + 2] == 'I')
             {
@@ -78,8 +86,7 @@ bool MIU::canApplyRule(const Rules rule, int *fromp)
     }
     case Rules::IV:
     {
-        int from = fromp ? *fromp : 0;
-        for (int i = from; i < m_size - 1; i++)
+        for (int i = *fromp; i < m_size - 1; i++)
         {
             if (m_data[i] == 'U' && m_data[i + 1] == 'U')
             {
@@ -94,68 +101,90 @@ bool MIU::canApplyRule(const Rules rule, int *fromp)
     return false;
 }
 
+bool MIU::canApplyRule(const Rules rule)
+{
+    int _ = 0;
+    return canApplyRule(rule, &_);
+}
+
+MIU MIU::applyRule(const Rules rule)
+{
+    int _ = 0;
+    return applyRule(rule, &_);
+}
+
 MIU MIU::applyRule(const Rules rule, int *fromp)
 {
-    int from = fromp ? *fromp : 0;
-    if (!canApplyRule(rule, &from))
+    if (!canApplyRule(rule, fromp))
         return MIU();
 
+    auto elem = MIU();
     switch (rule)
     {
     case Rules::I:
     {
-        char *newdata = (char *)malloc(m_size + 1);
+        char *newdata = (char *)malloc(sizeof(newdata) * (m_size + 2));
         strcpy(newdata, m_data);
         newdata[m_size] = 'U';
+        newdata[m_size + 1] = 0;
 
-        auto elem = MIU(newdata);
-        delete newdata;
-        return elem;
+        elem = MIU(newdata);
+        free(newdata);
+        break;
     }
     case Rules::II:
     {
-        char *newdata = (char *)malloc(2 * m_size - 1);
+        char *newdata = (char *)malloc(sizeof(newdata) * (2 * m_size));
         strcpy(newdata, m_data);
         for (int i = 0; i < m_size - 1; i++)
         {
             newdata[m_size + i] = m_data[i + 1];
         }
+        newdata[2 * m_size - 1] = 0;
 
-        auto elem = MIU(newdata);
-        delete newdata;
-        return elem;
+        elem = MIU(newdata);
+        free(newdata);
+        break;
     }
     case Rules::III:
     {
-        char *newdata = (char *)malloc(m_size);
+        char *newdata = (char *)malloc(sizeof(newdata) * m_size);
         strcpy(newdata, m_data);
-        newdata[from] = 'U';
-        for (int i = from + 1; i < m_size - 2; i++)
+        newdata[*fromp] = 'U';
+        for (int i = *fromp + 1; i < m_size - 2; i++)
         {
             newdata[i] = m_data[i + 2];
         }
         newdata[m_size - 2] = 0;
 
-        auto elem = MIU(newdata);
+        elem = MIU(newdata);
         free(newdata);
-        return elem;
+        break;
     }
     case Rules::IV:
     {
-        char *newdata = (char *)malloc(m_size);
+        char *newdata = (char *)malloc(sizeof(newdata) * m_size);
         strcpy(newdata, m_data);
-        for (int i = from; i < m_size - 2; i++)
+        for (int i = *fromp; i < m_size - 2; i++)
         {
             newdata[i] = m_data[i + 2];
         }
         newdata[m_size - 2] = 0;
 
-        auto elem = MIU(newdata);
+        elem = MIU(newdata);
         free(newdata);
-        return elem;
+        break;
     }
     }
+    *fromp += 1;
+    return elem;
+
     return MIU();
+}
+
+const char *MIU::getData()
+{
+    return m_data;
 }
 
 bool MIU::checkData(const char *data)
@@ -168,4 +197,10 @@ bool MIU::checkData(const char *data)
 bool MIU::isValid()
 {
     return m_size != -1;
+}
+
+void MIU::unsafeSetNull()
+{
+    m_data = nullptr;
+    m_size = 0;
 }
--- MIU/MIU.h
+++ MIU/MIU.h
@@ -6,6 +6,7 @@ enum class Rules
     II,
     III,
     IV,
+    Last
 };
 
 class MIU
@@ -16,13 +17,20 @@ public:
     ~MIU();
     MIU(const char *data);
     MIU &operator=(MIU from);
+    bool operator==(const MIU &oth);
 
     void copyFrom(const MIU &from);
     void setFromStr(const char *data);
-    bool canApplyRule(const Rules rule, int *from = nullptr);
-    MIU applyRule(const Rules rule, int *from = nullptr);
+
+    bool canApplyRule(const Rules rule, int *from);
+    bool canApplyRule(const Rules rule);
+    MIU applyRule(const Rules rule);
+    MIU applyRule(const Rules rule, int *from);
+
+    const char *getData();
     bool checkData(const char *data);
     bool isValid();
+    void unsafeSetNull();
 
 private:
     int m_size = -1;
